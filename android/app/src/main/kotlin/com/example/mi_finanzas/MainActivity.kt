f94509db5b9f5415cd2a632a639f9f305c78c724
Crisofer Jeyson <crisquispe196@gmail.com>
2026-09-13 00:51:59 -0500

    Revertir MainActivity a FlutterActivity para solucionar pantalla negra

--- android/app/src/main/kotlin/com/example/mi_finanzas/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mi_finanzas/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.mi_finanzas // MANTÉN TU PACKAGE ORIGINAL SI ES DISTINTO
+package com.example.mi_finanzas // OJO: MANTÉN TU PACKAGE ORIGINAL SI ERA DISTINTO
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }
